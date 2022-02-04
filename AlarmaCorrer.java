b9768c749d3b7d3b824fe9c17266b48de8bf4099
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2022-02-04 10:34:31 -0500

    Pruebas

--- AlarmaCorrer.java
+++ AlarmaCorrer.java
@@ -11,110 +11,32 @@ public class AlarmaCorrer {
          * Float16.toFloat16(3.14159265f))));
          * System.out.println("");
          */
-
         metodo16bits();
         // metodo32bits();
         // metodo64bits();
     }
 
     private static void metodo16bits() {
-        float pi = Float16.toFloat16(3.14159265f);
-
-        float DistanciaMedida = 0;
-        float Radio = Float16.toFloat16(0.5f); // en metros
-        float PerimetroRueda = Float16.toFloat16(2 * pi * Radio);
-        float DistanciaAlarma = Float16.toFloat16(5000f); // 5K o 10K
+        Float_16 f = new Float_16();
+        float pi =f.float16(3.14159265f);
+        float DistanciaMedida = 0f;
+        float Radio = f.float16(0.5f); // en metros
+        float PerimetroRueda = f.float16(2 * pi * Radio);
+        float DistanciaAlarma = f.float16(10000f); // 5K o 10K
         short NumeroGirosLlanta = 0;
-
-        float delta = Float16.toFloat16(pi);
-
-        String datosExportar = "Iteracion;Delta;Distancia\n";
-        String nombre = DistanciaAlarma + "- Multiplicacion" + "_16bits";
-
-        System.out.println("");
-
+        float delta = pi;
         while (true) {
-
-            NumeroGirosLlanta = Short.valueOf("" + (NumeroGirosLlanta + 1));
-            // float deltaActual = Float16.toFloat16(PerimetroRueda + DistanciaMedida) -
-            // DistanciaMedida;
-            // DistanciaMedida = Float16.toFloat16(DistanciaMedida + PerimetroRueda);
-            float deltaActual = Float16.toFloat16(PerimetroRueda * NumeroGirosLlanta) / NumeroGirosLlanta;
-            DistanciaMedida = Float16.toFloat16(NumeroGirosLlanta * PerimetroRueda);
-
-            if (delta != deltaActual) {
-                delta = deltaActual;
-
-                String bin = Integer.toBinaryString(Float16.encode(DistanciaMedida));
-
-                int exponente = Integer.parseInt(bin.substring(0, 5), 2) - 15;
-
-                System.out.println("delta: " + delta);
-                System.out.println("Exp: " + exponente);
-                System.out.println("Bin: " + toFormat(bin));
-                System.out.println("DistanciaMedida: " + DistanciaMedida);
-                System.out.println("NumeroGirosLlanta: " + NumeroGirosLlanta);
-                System.out.println("");
-
-                datosExportar += NumeroGirosLlanta + ";" + delta + ";" + DistanciaMedida + "\n";
-            }
-
-            if (DistanciaMedida >= DistanciaAlarma) {
-                System.out.println("=16=bits=================");
-                System.out.printf("¡Alarma!\n");
-                System.out.printf("DistanciaMedida: %f\n", DistanciaMedida);
-                System.out.printf("NumeroGirosLlanta: %d\n", NumeroGirosLlanta);
-                System.out.println("========================");
-
+            DistanciaMedida = f.float16(DistanciaMedida + PerimetroRueda);
+            System.out.println(f.getExp());
+            if((f.float16(DistanciaMedida + PerimetroRueda)-DistanciaMedida) == 0.0){
                 break;
             }
-
-            if (NumeroGirosLlanta == 32767) {
-                System.out.println("=16=bits================");
-                System.out.println("¡Short ha alcanzado el limite de " + Short.MAX_VALUE + "!");
-                System.out.printf("DistanciaMedida: %f\n", DistanciaMedida);
-                System.out.printf("NumeroGirosLlanta: %d\n", NumeroGirosLlanta);
-                System.out.println("========================");
+            NumeroGirosLlanta++;
+            if(DistanciaMedida == DistanciaAlarma){
+                System.out.println("Termino");
                 break;
             }
         }
-        System.out.println("");
-
-        ExportarDatos.exportar(nombre, datosExportar);
-
-    }
-
-    private static String toFormat(String bin) {
-
-        String temp = "0";
-
-        for (int i = 0; i < bin.length(); i++) {
-            if (i == 0 || i == 5)
-                temp += " ";
-            temp += bin.charAt(i);
-        }
-
-        return temp;
-    }
-
-    private static String toBin(int encode) {
-
-        String resultado = "";
-        for (int i = 0; i < 16; i++) {
-
-            // if (i >= 0 && i <= 4)
-            resultado += (encode % 2);
-            encode = encode / 2;
-
-            if (encode == 0)
-                break;
-        }
-
-        StringBuilder strb = new StringBuilder(resultado);
-        resultado = strb.reverse().toString();
-        resultado = resultado.substring(0, 5);
-
-        return resultado;
     }
 
     private static void metodo32bits() {
